3d126e98b2b424c3a5b974a0b5f6d577263c31d0
David Preininger <goljatpre@gmail.com>
2022-05-17 11:45:36 +0200

    gets the best-fit pose

--- merge.py
+++ merge.py
@@ -37,9 +37,40 @@ def compute_merged(path):
 
   merged = np.zeros((maxW, maxH, 3))
   
+  del_index = []
+  for index, img in enumerate(extracted_images):
+    if img.shape[0] < maxW/4 or img.shape[1] < maxH/4:
+      del_index.append(index)
+    else:
+      padded = cv2.resize(img, dsize=(np.shape(merged)[1], np.shape(merged)[0]))
+      merged = np.add(padded, merged)
+
+  for index in sorted(del_index, reverse=True):
+    del extracted_images[index]
+
+  # grayscale
+  merged = np.mean(merged, axis=2)
+
+  # normalization
+  merged_norm = np.zeros(np.shape(merged))
+  merged = cv2.normalize(merged, merged_norm, 0, 255, cv2.NORM_MINMAX)
+  merged_norm = np.round(merged_norm, 0).astype(np.uint8)
+
+  thresh = np.max(merged_norm) - 30
+  mask = np.zeros_like(merged_norm)
+  mask[merged_norm >= thresh] = 255
+
+  # get most similar image
+  best_image = None
+  best_score = 0
   for img in extracted_images:
-    padded = cv2.resize(img, dsize=(np.shape(merged)[1], np.shape(merged)[0]))
-    merged = np.add(padded, merged)
+    img = cv2.resize(img, dsize=(np.shape(mask)[1], np.shape(mask)[0]))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    score = np.sum(mask * img)
+    if score > best_score:
+      best_score = score
+      best_image = img
 
   # add gaussian blur
   gauss_size = int(min(np.shape(merged)[:2]) * 0.05)
@@ -47,16 +78,13 @@ def compute_merged(path):
     gauss_size += 1
   img = cv2.GaussianBlur(img, (gauss_size, gauss_size), 0)
 
-  # normalization
-  merged_norm = np.zeros(np.shape(merged))
-  merged = cv2.normalize(merged, merged_norm, 0, 255, cv2.NORM_MINMAX)
-
   # closing
-  merged = np.round(merged_norm, 0).astype(np.uint8)
-  merged = cv2.dilate(merged, np.ones((10, 10), np.uint8))
-  merged = cv2.erode(merged, np.ones((10, 10), np.uint8), iterations=2)
+  merged = cv2.dilate(merged, np.ones((15, 15), np.uint8))
+  merged = cv2.erode(merged, np.ones((15, 15), np.uint8), iterations=2)
 
-  plt.imshow(merged)
+  plt.imshow(best_image)
+  plt.savefig('{}/best.png'.format(path))
+  plt.imshow(merged_norm)
   plt.savefig('{}/merged.png'.format(path))
 
 
